e4657e78f83ba8d782562f7cd091725ab18d7085
z0d1akK <artemlinenko10@gmail.com>
2026-05-16 19:14:19 +0300

    refactor: edited UserSpecification

--- src/main/java/com/zodiakk/userservice/user/repository/specification/UserSpecification.java
+++ src/main/java/com/zodiakk/userservice/user/repository/specification/UserSpecification.java
@@ -8,18 +8,27 @@ public final class UserSpecification {
     private UserSpecification() { }
 
     public static Specification<User> hasName(String name) {
-        return (root, query, criteriaBuilder) ->
-                name == null ? null
-                        : criteriaBuilder.like(criteriaBuilder.lower(root.get("name")),
-                        "%" + name.toLowerCase() + "%"
-                );
+        return (root, query, criteriaBuilder) -> {
+            if (name == null || name.isBlank()) return criteriaBuilder.conjunction();
+            return criteriaBuilder.like(criteriaBuilder.lower(root.get("name")),
+                    "%" + name.toLowerCase() + "%"
+            );
+        };
     }
 
     public static Specification<User> hasSurname(String surname) {
-        return (root, query, criteriaBuilder) ->
-                surname == null ? null
-                        : criteriaBuilder.like(criteriaBuilder.lower(root.get("surname")),
-                        "%" + surname.toLowerCase() + "%"
-                );
+        return (root, query, criteriaBuilder) -> {
+            if (surname == null || surname.isBlank()) return criteriaBuilder.conjunction();
+            return criteriaBuilder.like(criteriaBuilder.lower(root.get("surname")),
+                    "%" + surname.toLowerCase() + "%"
+            );
+        };
+    }
+
+    public static Specification<User> hasActive(Boolean active) {
+        return (root, query, criteriaBuilder) -> {
+            if (active == null) return criteriaBuilder.conjunction();
+            return criteriaBuilder.equal(root.get("active"), active);
+        };
     }
 }
