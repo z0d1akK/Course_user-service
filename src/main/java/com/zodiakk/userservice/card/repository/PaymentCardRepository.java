cef1a912d9fb1c3fb66586e9c1a2a446a18e7838
z0d1akK <artemlinenko10@gmail.com>
2026-05-16 17:15:24 +0300

    refactor: added stereotype annotations on repositories

--- src/main/java/com/zodiakk/userservice/card/repository/PaymentCardRepository.java
+++ src/main/java/com/zodiakk/userservice/card/repository/PaymentCardRepository.java
@@ -7,10 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface PaymentCardRepository extends JpaRepository<PaymentCard, UUID> {
 
     List<PaymentCard> findAllByUserId(UUID userId);
--- src/main/java/com/zodiakk/userservice/user/repository/UserRepository.java
+++ src/main/java/com/zodiakk/userservice/user/repository/UserRepository.java
@@ -8,10 +8,12 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificationExecutor<User> {
 
     Optional<User> findByEmail(String email);
